05b98b68cd0b801c18c8b4eb82c1665e54d3f8cd
Daniil Poroshin <daniilporoshin@gmail.com>
2023-06-22 00:58:24 +0300

    refactor: use common test utils for 'devices upload-logs'

--- internal/app/enaptercli/cmd_devices_upload_logs_test.go
+++ internal/app/enaptercli/cmd_devices_upload_logs_test.go
@@ -1,11 +1,7 @@
 package enaptercli_test
 
 import (
-	"bytes"
 	"encoding/json"
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"strings"
@@ -32,83 +28,38 @@ func TestDeviceUploadLogs(t *testing.T) {
 	}
 }
 
+type devicesUploadLogsTestSettings struct {
+	OperationID string `json:"operation_id"`
+	HardwareID  string `json:"hardware_id"`
+	CliMessage  string `json:"cli_message"`
+	Token       string `json:"-"`
+}
+
+func (s *devicesUploadLogsTestSettings) Fill(t *testing.T, dir string) {
+	settingsBytes, err := os.ReadFile(filepath.Join(dir, "settings.json"))
+	require.NoError(t, err)
+	require.NoError(t, json.Unmarshal(settingsBytes, s))
+	s.Token = faker.Word()
+}
+
 func testDeviceUploadLogs(t *testing.T, dir string) {
-	testSettings := parseTestSettings(t, dir)
-	srv := startTestServer(testSettings)
+	var settings devicesUploadLogsTestSettings
+	settings.Fill(t, dir)
+
+	reqs := byteSliceSliceFromFile(t, filepath.Join(dir, "requests"))
+	resps := byteSliceSliceFromFile(t, filepath.Join(dir, "responses"))
+
+	srv := startTestServer(reqs, resps, settings.CliMessage)
 	defer srv.Close()
 
 	args := strings.Split("enapter devices upload-logs", " ")
 	args = append(args,
-		"--token", testSettings.Token,
-		"--hardware-id", testSettings.HardwareID,
+		"--token", settings.Token,
+		"--hardware-id", settings.HardwareID,
 		"--gql-api-url", srv.URL)
-	if testSettings.OperationID != "" {
-		args = append(args, "--operation-id", testSettings.OperationID)
-	}
-	app := startTestApp(args...)
-	defer app.Stop()
-
-	appErr := app.Wait()
-
-	actual, err := io.ReadAll(app.Stdout())
-	require.NoError(t, err)
-
-	if appErr != nil {
-		actual = append(actual, []byte("app exit with error: "+appErr.Error()+"\n")...)
+	if settings.OperationID != "" {
+		args = append(args, "--operation-id", settings.OperationID)
 	}
 
-	expectedFileName := filepath.Join(dir, "output")
-	if update {
-		err := os.WriteFile(expectedFileName, actual, 0o600)
-		require.NoError(t, err)
-	}
-
-	expected, err := os.ReadFile(expectedFileName)
-	require.NoError(t, err)
-
-	require.Equal(t, string(expected), string(actual))
-}
-
-type testSettings struct {
-	OperationID string   `json:"operation_id"`
-	HardwareID  string   `json:"hardware_id"`
-	CliMessage  string   `json:"cli_message"`
-	Token       string   `json:"-"`
-	Responses   [][]byte `json:"-"`
-}
-
-func parseTestSettings(t *testing.T, dir string) testSettings {
-	var testSettings testSettings
-	settingsBytes, err := os.ReadFile(filepath.Join(dir, "settings.json"))
-	require.NoError(t, err)
-	require.NoError(t, json.Unmarshal(settingsBytes, &testSettings))
-
-	uploadResp, err := os.ReadFile(filepath.Join(dir, "upload_logs_resp"))
-	require.NoError(t, err)
-
-	testSettings.Responses = bytes.Split(uploadResp, []byte{'\n'})
-	testSettings.Token = faker.Word()
-
-	return testSettings
-}
-
-func startTestServer(settings testSettings) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		for _, resp := range settings.Responses {
-			if settings.CliMessage != "" {
-				w.Header().Set("X-ENAPTER-CLI-MESSAGE", settings.CliMessage)
-			}
-
-			if len(resp) == 0 {
-				continue
-			}
-
-			_, _ = w.Write(resp)
-			settings.Responses = settings.Responses[1:]
-			return
-		}
-
-		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte("to much requests for test"))
-	}))
+	checkTestAppOutput(t, dir, args, reqs)
 }
