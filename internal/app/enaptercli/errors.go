e60e8f8b227433adb7d5a7ee13cda18ed05fd46e
Daniil Poroshin <daniilporoshin@gmail.com>
2023-07-14 12:58:09 +0300

    refactor(cli): move upload logs to cloudapi

--- internal/app/enaptercli/errors.go
+++ internal/app/enaptercli/errors.go
@@ -4,7 +4,6 @@ import "errors"
 
 var (
 	errFinishedWithError = errors.New("request execution failed")
-	errLogStatusError    = errors.New("error during request execution")
 	errAPITokenMissed    = errors.New("API token missing. Set it up using environment " +
 		"variable ENAPTER_API_TOKEN")
 	errRequestTimedOut = errors.New("request timed out")
