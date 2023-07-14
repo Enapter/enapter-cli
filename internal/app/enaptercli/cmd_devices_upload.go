4850dca1f22ecaafd2dd4cc333bb633bf11958d9
Daniil Poroshin <daniilporoshin@gmail.com>
2023-07-14 12:00:51 +0300

    refactor(cli): move blueprints uploading to cloudapi

--- internal/app/enaptercli/cmd_devices_upload.go
+++ internal/app/enaptercli/cmd_devices_upload.go
@@ -4,51 +4,25 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"time"
 
-	"github.com/shurcooL/graphql"
 	"github.com/urfave/cli/v2"
+
+	"github.com/enapter/enapter-cli/internal/cloudapi"
 )
 
 const deviceUploadDefaultTimeout = 30 * time.Second
 
 type cmdDevicesUpload struct {
-	cmdDevicesUploadCommon
-	blueprintDir string
-}
-
-type cmdDevicesUploadCommon struct {
 	cmdDevices
-	timeout time.Duration
-}
-
-func (c *cmdDevicesUploadCommon) Flags() []cli.Flag {
-	flags := c.cmdDevices.Flags()
-	flags = append(flags,
-		&cli.DurationFlag{
-			Name:        "timeout",
-			Usage:       "Time to wait for blueprint uploading",
-			Destination: &c.timeout,
-			Value:       deviceUploadDefaultTimeout,
-		},
-	)
-	return flags
+	blueprintDir string
+	timeout      time.Duration
 }
 
 func buildCmdDevicesUpload() *cli.Command {
 	cmd := &cmdDevicesUpload{}
-
-	flags := cmd.Flags()
-	flags = append(flags, &cli.StringFlag{
-		Name:        "blueprint-dir",
-		Usage:       "Directory which contains blueprint file",
-		Required:    true,
-		Destination: &cmd.blueprintDir,
-	})
 
 	return &cli.Command{
 		Name:  "upload",
@@ -57,7 +31,7 @@ func buildCmdDevicesUpload() *cli.Command {
 			"The command updates device blueprint and uploads the firmware to the UCM. Learn more " +
 			"about Enapter Blueprints at https://handbook.enapter.com/blueprints.",
 		CustomHelpTemplate: cmd.HelpTemplate(),
-		Flags:              flags,
+		Flags:              cmd.Flags(),
 		Before:             cmd.Before,
 		Action: func(cliCtx *cli.Context) error {
 			return cmd.upload(cliCtx.Context, cliCtx.App.Version)
@@ -65,33 +39,23 @@ func buildCmdDevicesUpload() *cli.Command {
 	}
 }
 
-// UploadBlueprintInput contains mutation input variables.
-type UploadBlueprintInput struct {
-	Blueprint  graphql.String `json:"blueprint"`
-	HardwareID graphql.ID     `json:"hardwareId"`
-}
-
-type mutation struct {
-	Device *struct {
-		UploadBlueprint struct {
-			Data   uploadBlueprintData
-			Errors []uploadBlueprintError
-		} `graphql:"uploadBlueprint(input: $input)"`
-	}
-}
-
-type uploadBlueprintError struct {
-	Code    string
-	Message string
-	Path    []string
-	Title   string
-}
-
-type uploadBlueprintData struct {
-	Code        string
-	Message     string
-	Title       string
-	OperationID string
+func (c *cmdDevicesUpload) Flags() []cli.Flag {
+	flags := c.cmdDevices.Flags()
+	flags = append(flags,
+		&cli.DurationFlag{
+			Name:        "timeout",
+			Usage:       "Time to wait for blueprint uploading",
+			Destination: &c.timeout,
+			Value:       deviceUploadDefaultTimeout,
+		},
+		&cli.StringFlag{
+			Name:        "blueprint-dir",
+			Usage:       "Directory which contains blueprint file",
+			Required:    true,
+			Destination: &c.blueprintDir,
+		},
+	)
+	return flags
 }
 
 func (c *cmdDevicesUpload) upload(ctx context.Context, version string) error {
@@ -101,40 +65,40 @@ func (c *cmdDevicesUpload) upload(ctx context.Context, version string) error {
 		defer cancel()
 	}
 
-	zipBuf, err := c.blueprintZipBuf()
+	zipBytes, err := c.blueprintZip()
 	if err != nil {
 		return err
 	}
 
 	onceWriter := &onceWriter{w: c.writer}
-	m, err := c.sendRequest(ctx, zipBuf, onceWriter, version)
+	transport := cloudapi.NewCredentialsTransport(http.DefaultTransport, c.token, version)
+	transport = cloudapi.NewCLIMessageWriterTransport(transport, onceWriter)
+	client := cloudapi.NewClientWithURL(&http.Client{Transport: transport}, c.graphqlURL)
+
+	uploadData, uploadErrors, err := client.UploadBlueprint(ctx, c.hardwareID, zipBytes)
 	if err != nil {
-		return fmt.Errorf("failed to send request: %w", err)
+		return fmt.Errorf("do update: %w", err)
 	}
 
-	if m.Device == nil {
-		return fmt.Errorf("device not found: %w", errFinishedWithError)
-	}
-
-	if len(m.Device.UploadBlueprint.Errors) != 0 {
-		c.dumpUploadErrors(m.Device.UploadBlueprint.Errors)
+	if len(uploadErrors) != 0 {
+		for _, e := range uploadErrors {
+			fmt.Fprintln(c.writer, "[ERROR]", e.Message)
+		}
 		return errFinishedWithError
 	}
 
-	fmt.Fprintln(c.writer, "upload started with operation id", m.Device.UploadBlueprint.Data.OperationID)
+	fmt.Fprintln(c.writer, "upload started with operation id", uploadData.OperationID)
 
-	op := cmdDevicesUploadLogs{
-		cmdDevicesUploadCommon: c.cmdDevicesUploadCommon,
-	}
-	if err := op.logs(ctx, m.Device.UploadBlueprint.Data.OperationID, onceWriter, version); err != nil {
-		return err
+	err = client.WriteOperationLogs(ctx, c.hardwareID, uploadData.OperationID, c.writeLog)
+	if err != nil {
+		return fmt.Errorf("receive operation logs: %w", err)
 	}
 
 	fmt.Fprintln(c.writer, "Done!")
 	return nil
 }
 
-func (c *cmdDevicesUpload) blueprintZipBuf() (*bytes.Buffer, error) {
+func (c *cmdDevicesUpload) blueprintZip() ([]byte, error) {
 	bpBytes, err := zipDir(c.blueprintDir)
 	if err != nil {
 		return nil, fmt.Errorf("failed to zip blueprint dir %q: %w", c.blueprintDir, err)
@@ -152,86 +116,9 @@ func (c *cmdDevicesUpload) blueprintZipBuf() (*bytes.Buffer, error) {
 		return nil, fmt.Errorf("failed to encode blueprint as base64: %w", err)
 	}
 
-	return zipBuf, nil
+	return zipBuf.Bytes(), nil
 }
 
-func (c *cmdDevicesUpload) sendRequest(
-	ctx context.Context, blueprintBuf *bytes.Buffer, onceWriter *onceWriter, version string,
-) (*mutation, error) {
-	extraHeaders := map[string][]string{
-		"Authorization":         {"Bearer " + c.token},
-		"X-ENAPTER-CLI-VERSION": {version},
-	}
-	httpClient := &http.Client{
-		Transport: extraHeaderRoundTripper{
-			tripper: cliMessageRoundTripper{
-				tripper: http.DefaultTransport,
-				writer:  onceWriter,
-			},
-			extraHeaders: extraHeaders,
-		},
-	}
-
-	client := graphql.NewClient(c.graphqlURL, httpClient)
-
-	var m mutation
-	variables := map[string]interface{}{
-		"input": UploadBlueprintInput{
-			Blueprint:  graphql.String(blueprintBuf.String()),
-			HardwareID: graphql.String(c.hardwareID),
-		},
-	}
-
-	if err := client.Mutate(ctx, &m, variables); err != nil {
-		if errors.Is(err, context.DeadlineExceeded) {
-			err = errRequestTimedOut
-		}
-		return nil, err
-	}
-
-	return &m, nil
-}
-
-func (c *cmdDevicesUpload) dumpUploadErrors(errs []uploadBlueprintError) {
-	for _, e := range errs {
-		fmt.Fprintln(c.writer, "[ERROR]", e.Message)
-	}
-}
-
-type extraHeaderRoundTripper struct {
-	tripper      http.RoundTripper
-	extraHeaders map[string][]string
-}
-
-func (e extraHeaderRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	newReq := new(http.Request)
-	*newReq = *r
-
-	newReq.Header = make(http.Header, len(r.Header)+len(e.extraHeaders))
-	for k, s := range r.Header {
-		newReq.Header[k] = s
-	}
-	for k, s := range e.extraHeaders {
-		newReq.Header[k] = s
-	}
-
-	return e.tripper.RoundTrip(newReq)
-}
-
-type cliMessageRoundTripper struct {
-	tripper http.RoundTripper
-	writer  io.Writer
-}
-
-func (c cliMessageRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	resp, err := c.tripper.RoundTrip(r)
-	if err != nil {
-		return nil, err
-	}
-
-	if msg := resp.Header.Get("X-ENAPTER-CLI-MESSAGE"); msg != "" {
-		fmt.Fprintln(c.writer, msg)
-	}
-
-	return resp, nil
+func (c *cmdDevicesUpload) writeLog(operationID string, l cloudapi.OperationLog) {
+	fmt.Fprintf(c.writer, "[#%s] %s [%s] %s\n", operationID, l.CreatedAt, l.Severity, l.Payload)
 }
--- internal/app/enaptercli/errors.go
+++ internal/app/enaptercli/errors.go
@@ -6,5 +6,4 @@ var (
 	errFinishedWithError = errors.New("request execution failed")
 	errAPITokenMissed    = errors.New("API token missing. Set it up using environment " +
 		"variable ENAPTER_API_TOKEN")
-	errRequestTimedOut = errors.New("request timed out")
 )
