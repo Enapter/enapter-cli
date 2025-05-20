c7da9d782b47630d6fd1a728c901e3cff7ceb83b
Aleksey Bakin <alexey.bakin@gmail.com>
2025-05-20 10:35:19 +0300

    feat: assign-blueprint and provision lua-device with blueprint from path

--- internal/app/enaptercli/cmd_blueprints_upload.go
+++ internal/app/enaptercli/cmd_blueprints_upload.go
@@ -3,6 +3,7 @@ package enaptercli
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -41,25 +42,54 @@ func (c *cmdBlueprintsUpload) Flags() []cli.Flag {
 }
 
 func (c *cmdBlueprintsUpload) upload(ctx context.Context) error {
-	fi, err := os.Stat(c.blueprintPath)
+	return uploadBlueprint(ctx, c.blueprintPath, c.doHTTPRequest)
+}
+
+func uploadBlueprintAndReturnBlueprintID(ctx context.Context, blueprintPath string,
+	doHTTPRequest func(context.Context, doHTTPRequestParams) error,
+) (string, error) {
+	var blueprintID string
+	err := uploadBlueprint(ctx, blueprintPath, func(ctx context.Context, reqParams doHTTPRequestParams) error {
+		reqParams.RespProcessor = func(resp *http.Response) error {
+			var respBlueprint struct {
+				Blueprint struct {
+					ID string `json:"id"`
+				} `json:"blueprint"`
+			}
+			if err := json.NewDecoder(resp.Body).Decode(&respBlueprint); err != nil {
+				return fmt.Errorf("decode blueprint response: %w", err)
+			}
+			blueprintID = respBlueprint.Blueprint.ID
+			return nil
+		}
+		return doHTTPRequest(ctx, reqParams)
+	})
+	return blueprintID, err
+}
+
+func uploadBlueprint(
+	ctx context.Context, blueprintPath string,
+	doHTTPRequest func(context.Context, doHTTPRequestParams) error,
+) error {
+	fi, err := os.Stat(blueprintPath)
 	if err != nil {
 		return fmt.Errorf("check blueprint path: %w", err)
 	}
 
 	var data []byte
 	if fi.IsDir() {
-		data, err = zipDir(c.blueprintPath)
+		data, err = zipDir(blueprintPath)
 		if err != nil {
 			return fmt.Errorf("zip blueprint directory: %w", err)
 		}
 	} else {
-		data, err = os.ReadFile(c.blueprintPath)
+		data, err = os.ReadFile(blueprintPath)
 		if err != nil {
 			return fmt.Errorf("read blueprint zip file: %w", err)
 		}
 	}
 
-	return c.doHTTPRequest(ctx, doHTTPRequestParams{
+	return doHTTPRequest(ctx, doHTTPRequestParams{
 		Method: http.MethodPost,
 		Path:   "/blueprints/upload",
 		Body:   bytes.NewReader(data),
