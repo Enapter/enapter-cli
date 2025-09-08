97a3dfa81f6ff50334d605351f7252aecc14e74b
Daniil Poroshin <daniilporoshin@gmail.com>
2025-09-08 20:14:03 +0400

    feat: print downloaded file name

--- internal/app/enaptercli/cmd_blueprints_download.go
+++ internal/app/enaptercli/cmd_blueprints_download.go
@@ -90,6 +90,7 @@ func (c *cmdBlueprintsDownload) do(ctx context.Context) error {
 			if _, err := io.Copy(outFile, body); err != nil {
 				return fmt.Errorf("write output file %q: %w", c.outputFileName, err)
 			}
+			fmt.Fprintln(c.writer, c.outputFileName)
 			return nil
 		}),
 	})
--- internal/app/enaptercli/cmd_blueprints_profiles_download.go
+++ internal/app/enaptercli/cmd_blueprints_profiles_download.go
@@ -56,6 +56,7 @@ func (c *cmdBlueprintsProfilesDownload) do(ctx context.Context) error {
 			if _, err := io.Copy(outFile, body); err != nil {
 				return fmt.Errorf("write output file %q: %w", c.outputFileName, err)
 			}
+			fmt.Fprintln(c.writer, c.outputFileName)
 			return nil
 		}),
 	})
