50ab04d67b57dc43746749b3b6e4916495f1e8dc
Daniil Poroshin <daniilporoshin@gmail.com>
2026-01-12 16:59:19 +0400

    feat: improve missing site id error message

--- internal/app/enaptercli/errors.go
+++ internal/app/enaptercli/errors.go
@@ -3,8 +3,9 @@ package enaptercli
 import "errors"
 
 var (
-	errSiteIDMismatch       = errors.New("passed site-ID must match the site-ID of the current connection")
-	errSiteIDMissing        = errors.New("site ID is required")
+	errSiteIDMismatch = errors.New("passed site-ID must match the site-ID of the current connection")
+	errSiteIDMissing  = errors.New("site ID is required, " +
+		"specify --site-id or select a connection with a configured site ID")
 	errUnsupportedFlagValue = errors.New("unsupported flag value")
 	errOnlyOneBlueprinFlag  = errors.New("only one of --blueprint-id or --blueprint-path can be specified")
 	errMissedBlueprintFlag  = errors.New("one of --blueprint-id or --blueprint-path must be specified")
